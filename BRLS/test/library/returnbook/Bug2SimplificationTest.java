b8cdef071e8edb6260a5a40ce60d1f215e696827
Cezanne <cjalamgir@gmail.com>
2020-10-11 23:03:37 +1100

    Reset bug in Calendar; added bug 2 log; designed bug 2 test script

---
+++ BRLS/test/library/returnbook/Bug2SimplificationTest.java
@@ -0,0 +1,14 @@
+package library.returnbook;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Bug2SimplificationTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
